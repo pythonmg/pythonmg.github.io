25e925cf80a430f678eb8e1d684aac1c3ebdb68f
Cássio Botaro <cassiobotaro@gmail.com>
2016-09-28 08:58:58 -0300

    Fix telegram url. Fixed #2

--- pelicanconf.py
+++ pelicanconf.py
@@ -153,7 +153,7 @@ SOCIAL_LINKS = (
         "text": "Mailing List",
     },
     {
-        "href": "#",
+        "href": "https://telegram.me/joinchat/ADyifgM5UvBM95seWv33pA",
         "icon": "fa-paper-plane",
         "text": "Telegram",
     },
@@ -241,7 +241,7 @@ MALT_COMUNITY = [
             },
             {
                 'text': '<i class="fa fa-paper-plane fa-2x"></i> Telegram',
-                'href': 'https://pythonmg.github.io'
+                'href': 'https://telegram.me/joinchat/ADyifgM5UvBM95seWv33pA'
             },
         ],
     },
